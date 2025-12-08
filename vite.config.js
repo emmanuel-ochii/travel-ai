6b2bb9b9baf4dcd8c9c83c21ef1600186898b7c3
emmanuel-ochii <emmanuelochubili@gmail.com>
2025-12-08 02:06:24 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',  // <--- Required for Laravel on Render
+        emptyOutDir: true,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-});
+})
